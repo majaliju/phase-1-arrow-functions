793664e2aa4a1f7d47aabf2b91a6c2432ac0dbf6
majaliju <majalijunyc@gmail.com>
2022-01-18 19:15:14 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (num1, num2) =>  2000/100;
+
+const square = (num1, num2) => {
+    if (num2 !== true) {
+        return num1 ** num1;
+    }
+    else {
+        return num1 ** num2;
+    }
+}
+
+const add = (num1, num2) => num1 + num2 ;
